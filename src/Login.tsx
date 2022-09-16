a930f338edcbc8a78a48f9f2fb6dfd8bf01e7c24
Dante <danteferronato@epet12smandes.edu.ar>
2022-09-16 16:22:56 -0300

    Partially built login

--- src/Login.tsx
+++ src/Login.tsx
@@ -4,31 +4,15 @@ import "./style/login.css"
 export default function Login() {
     return (
         <section className="login">
-            <div className="e1_2">
-                <div className="e36_3" />
-                <div className="e36_4" />
-                <div className="e40_11">
-                    <div className="e40_12" />
-                    <span  className="e40_13">Username</span>
-                    <div className="e40_14">
-                        <div className="e40_15" />
-                        <div className="e40_16" />
-                    </div>
-                </div>
-                <div className="e40_17">
-                    <div className="e40_18" />
-                    <span  className="e40_19">password</span>
-                    <div className="e40_20">
-                        <div className="e40_21" />
-                        <div className="e40_22" />
-                    </div>
-                </div>
-                <div className="e40_23">
-                    <div className="e40_24" />
-                    <span  className="e40_25">login</span>
-                </div>
-                <span  className="e40_26">Forgot password?</span>
+            <div className="header">
+                <div className="title" />
             </div>
+            <form className="login-input">
+                <input type="text" id="" placeholder="Username" />
+                <input type="password" id="" placeholder="Password" />
+                <input type="submit" value="Login" />
+                <a>Forgot password?</a>
+            </form>
         </section>
     )
 }
